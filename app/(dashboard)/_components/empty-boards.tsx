ff2340d7dacb6f36b2a05c6d7318f21496775214
4meyDam1e <amey.damle@mail.utoronto.ca>
2024-04-09 13:05:49 -0400

    Setup convex backed, implemented basic board create api

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,9 +1,30 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { toast } from "sonner";
+import { useOrganization } from "@clerk/nextjs";
+
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 
 export const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+    mutate({
+      orgId: organization.id,
+      title: "Untitled",
+    })
+      .then((id) => {
+        toast.success("Board created");
+        // TODO: Redirect to board/{id}
+      })
+      .catch(() => toast.error("Failed to create board"));
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image src="/note.svg" alt="Empty Favourites" height={140} width={140} />
@@ -15,7 +36,9 @@ export const EmptyBoards = () => {
       </p>
 
       <div className="mt-6">
-        <Button size="lg">Create board</Button>
+        <Button disabled={pending} onClick={onClick} size="lg">
+          Create board
+        </Button>
       </div>
     </div>
   );
