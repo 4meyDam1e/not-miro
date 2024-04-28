29a39edce0458dfcee32f5ad1ac17c229c4d40a4
4meyDam1e <amey.damle@mail.utoronto.ca>
2024-04-28 02:03:08 -0400

    Added card actions - copy link, rename and delete

--- convex/board.ts
+++ convex/board.ts
@@ -29,14 +29,52 @@ export const create = mutation({
 
     const randomImage = images[Math.floor(Math.random() * images.length)];
 
-    console.log(randomImage, "TEST");
-
     const board = await ctx.db.insert("boards", {
       title: args.title,
       orgId: args.orgId,
       authorId: identity.subject,
       authorName: identity.name!,
       imageUrl: randomImage,
+    });
+
+    return board;
+  },
+});
+
+export const remove = mutation({
+  args: { id: v.id("boards") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    // TODO: Later check to delete favourite relation as well
+
+    await ctx.db.delete(args.id);
+  },
+});
+
+export const update = mutation({
+  args: { id: v.id("boards"), title: v.string() },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    const title = args.title.trim();
+
+    if (!title) {
+      throw new Error("Title is required");
+    } else if (title.length > 60) {
+      throw new Error("Title cannot be longer than 60 characters");
+    }
+
+    const board = await ctx.db.patch(args.id, {
+      title: args.title,
     });
 
     return board;
