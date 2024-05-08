b2fdd570f7d70b6f68a5c51044f691e9da89d375
4meyDam1e <amey.damle@mail.utoronto.ca>
2024-05-07 22:45:41 -0400

    Implemented info and participants sections

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,12 +1,53 @@
 "use client";
 
+import { useOthers, useSelf } from "@/liveblocks.config";
+import { connectionIdToColor } from "@/lib/utils";
+
+import { UserAvatar } from "./user-avatar";
+
+const MAX_SHOWN_OTHER_USERS = 1;
+
 export const Participants = () => {
+  const otherUsers = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = otherUsers.length > MAX_SHOWN_OTHER_USERS;
+
   return (
     <div
       className="absolute h-12 top-2 right-2 bg-white rounded-md
       p-3 flex items-center shadow-md"
     >
-      List of users
+      <div className="flex gap-x-2">
+        {currentUser && (
+          <UserAvatar
+            src={currentUser.info?.picture}
+            name={`${currentUser.info?.name} (You)`}
+            fallback={currentUser.info?.name?.[0]}
+            borderColor={connectionIdToColor(currentUser.connectionId)}
+          />
+        )}
+
+        {otherUsers.slice(0, MAX_SHOWN_OTHER_USERS)
+          .map(({ connectionId, info }) => {
+            return (
+              <UserAvatar
+                key={connectionId}
+                src={info?.picture}
+                name={info?.name}
+                fallback={info?.name?.[0] || "T"}
+                borderColor={connectionIdToColor(connectionId)}
+              />
+            );
+          })
+        }
+
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`${otherUsers.length - MAX_SHOWN_OTHER_USERS} more`}
+            fallback={`+${otherUsers.length - MAX_SHOWN_OTHER_USERS}`}
+          />
+        )}
+      </div>
     </div>
   );
 };
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,28 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+const COLORS = [
+  "#DC2626",
+  "#D97706",
+  "#059669",
+  "#7C3AED",
+  "#DB2777",
+  "#0D9488",
+  "#F59E0B",
+  "#3B82F6",
+  "#7F1D1D",
+  "#34D399",
+  "#7C3AED",
+  "#EF4444",
+  "#F472B6",
+  "#34D399",
+  "#10B981",
+];
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length]
 }
